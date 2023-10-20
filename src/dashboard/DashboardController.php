4fd562cb9d50ce7a83eb0c6aa0924c5da0356e60
Наранхүү <codesaur@gmail.com>
2023-10-20 11:19:43 +0800

    template simplify

--- src/dashboard/DashboardController.php
+++ src/dashboard/DashboardController.php
@@ -141,58 +141,52 @@ class DashboardController extends \Raptor\Controller
     protected function createDefaultMenu()
     {
         try {
-            $pattern = '/record?model=' . MenuModel::class;
-
-            $main_id = $this->indopost($pattern, ['content' => ['mn' => ['title' => 'Үндсэн'], 'en' => ['title' => 'Main']], 'record' => ['position' => '10']]);
-            $this->indopost($pattern, [
-                'content' => ['mn' => ['title' => 'Хянах самбар'], 'en' => ['title' => 'Dashboard']],
-                'record' => ['parent_id' => $main_id, 'position' => '12', 'icon' => 'bi bi-easel', 'href' => $this->generateLink('home')]
-            ]);
-
-            $contents_id = $this->indopost($pattern, ['content' => ['mn' => ['title' => 'Агуулгууд'], 'en' => ['title' => 'Contents']], 'record' => ['position' => '200']]);
-            $this->indopost($pattern, [ 
+            $recordMenu = '/record?model=' . MenuModel::class;
+            
+            $contents_id = $this->indopost($recordMenu, ['content' => ['mn' => ['title' => 'Агуулгууд'], 'en' => ['title' => 'Contents']], 'record' => ['position' => '200']]);
+            $this->indopost($recordMenu, [ 
                 'content' => ['mn' => ['title' => 'Хуудсууд'], 'en' => ['title' => 'Pages']],
                 'record' => ['parent_id' => $contents_id, 'position' => '260', 'alias' => 'system', 'permission' => 'system_content_index', 'icon' => 'bi bi-book-half', 'href' => $this->generateLink('pages')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Мэдээнүүд'], 'en' => ['title' => 'News']],
                 'record' => ['parent_id' => $contents_id, 'position' => '270', 'alias' => 'system', 'permission' => 'system_content_index', 'icon' => 'bi bi-newspaper', 'href' => $this->generateLink('news')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Файлууд'], 'en' => ['title' => 'Files']],
                 'record' => ['parent_id' => $contents_id, 'position' => '275', 'alias' => 'system', 'permission' => 'system_content_index', 'icon' => 'bi bi-folder', 'href' => $this->generateLink('files')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Хэл'], 'en' => ['title' => 'Languages']],
                 'record' => ['parent_id' => $contents_id, 'position' => '280', 'alias' => 'system', 'permission' => 'system_localization_index', 'icon' => 'bi bi-flag-fill', 'href' => $this->generateLink('languages')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Текстүүд'], 'en' => ['title' => 'Texts']],
                 'record' => ['parent_id' => $contents_id, 'position' => '285', 'alias' => 'system', 'permission' => 'system_localization_index', 'icon' => 'bi bi-translate', 'href' => $this->generateLink('texts')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Лавлах хүснэгтүүд'], 'en' => ['title' => 'Reference Tables']],
                 'record' => ['parent_id' => $contents_id, 'position' => '290', 'alias' => 'system', 'permission' => 'system_templates_index', 'icon' => 'bi bi-layout-wtf', 'href' => $this->generateLink('references')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Дэлхийн улсууд'], 'en' => ['title' => 'World countries']],
                 'record' => ['parent_id' => $contents_id, 'position' => '295', 'alias' => 'system', 'permission' => 'system_localization_index', 'icon' => 'bi bi-flag', 'href' => $this->generateLink('countries')]
             ]);
             
-            $system_id = $this->indopost($pattern, ['content' => ['mn' => ['title' => 'Систем'], 'en' => ['title' => 'System']], 'record' => ['position' => '300']]);
-            $this->indopost($pattern, [
+            $system_id = $this->indopost($recordMenu, ['content' => ['mn' => ['title' => 'Систем'], 'en' => ['title' => 'System']], 'record' => ['position' => '300']]);
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Хэрэглэгчид'], 'en' => ['title' => 'Accounts']],
                 'record' => ['parent_id' => $system_id, 'position' => '310', 'alias' => 'system', 'permission' => 'system_account_index', 'icon' => 'bi bi-people-fill', 'href' => $this->generateLink('accounts')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Байгууллагууд'], 'en' => ['title' => 'Organizations']],
                 'record' => ['parent_id' => $system_id, 'position' => '320', 'alias' => 'system', 'permission' => 'system_organization_index', 'icon' => 'bi bi-building', 'href' => $this->generateLink('organizations')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Тохируулгууд'], 'en' => ['title' => 'Settings']],
                 'record' => ['parent_id' => $system_id, 'position' => '330', 'alias' => 'system', 'permission' => 'system_content_settings', 'icon' => 'bi bi-gear-wide-connected', 'href' => $this->generateLink('settings')]
             ]);
-            $this->indopost($pattern, [
+            $this->indopost($recordMenu, [
                 'content' => ['mn' => ['title' => 'Хандалтын протокол'], 'en' => ['title' => 'Access logs']],
                 'record' => ['parent_id' => $system_id, 'position' => '340', 'alias' => 'system', 'permission' => 'system_logger', 'icon' => 'bi bi-list-stars', 'href' => $this->generateLink('logs')]
             ]);
